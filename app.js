66cf8d918030189539c9bd951a4b088e5d5d7c1c
Bhumika2101 <bhumikasalunkhe283@gmail.com>
2025-05-05 18:32:46 +0530

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const nodemailer = require('nodemailer');
+const { request } = require('http');
+require('dotenv').config();
+
 
 const app = express();
 
@@ -18,10 +21,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.render('contact');
+  res.render('contact');  
 });
 
-app.post('/send', (req, res) => {
+app.post('/send', (req, res) => { 
+ 
   const output = `
     <p>You have a new contact request</p>
     <h3>Contact Details</h3>
@@ -34,25 +38,27 @@ app.post('/send', (req, res) => {
     <h3>Message</h3>
     <p>${req.body.message}</p>
   `;
+  console.log("send requested data:", req.body);
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'mail.YOURDOMAIN.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
-        user: 'YOUREMAIL', // generated ethereal user
-        pass: 'YOURPASSWORD'  // generated ethereal password
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS  // generated ethereal password
     },
     tls:{
       rejectUnauthorized:false
     }
   });
 
+  const reciverEmail = req.body.email; 
   // setup email data with unicode symbols
   let mailOptions = {
-      from: '"Nodemailer Contact" <your@email.com>', // sender address
-      to: 'RECEIVEREMAILS', // list of receivers
+      from: '"Nodemailer Contact" <bhumika.rcpit@gmail.com>', // sender address
+      to: reciverEmail, // list of receivers
       subject: 'Node Contact Request', // Subject line
       text: 'Hello world?', // plain text body
       html: output // html body
